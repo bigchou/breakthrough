037aaf4493f361aaed7b894827544913a72a94df
bigchou <40347905S@gmail.com>
2017-05-07 21:01:17 +0800

    pop&count

--- breakthrough/bitboard.h
+++ breakthrough/bitboard.h
@@ -10,6 +10,13 @@
 #define bitboard_h
 #include "board.h"
 
+const int BitTable[64] = {
+    63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
+    51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52,
+    26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28,
+    58, 20, 37, 17, 36, 8
+};
+
 void PrintBitBoard(US64 bb,int* Sq120ToSq64){
     US64 shiftMe = 1ULL;
     int rank = 0;
@@ -34,7 +41,26 @@ void PrintBitBoard(US64 bb,int* Sq120ToSq64){
         printf("\n");
     }
     printf("\n\n");
-    
 }
+
+int countBits(US64 b){
+    int r;
+    for(r=0;b;r++,b&= (b-1)){
+        //printf("%llu\n",b&(b-1));
+        //printf("%d\n",r);
+    }
+    //b = 200ULL;
+    //b&=(b-1);
+    //printf("%llu\n",b);
+    return r;
+}
+
+int PopBit(US64 &bb){
+    US64 b = bb ^ (bb-1);
+    unsigned int fold = (unsigned) ((b&0xffffffff)^(b >> 32));
+    bb &= (bb-1);
+    return BitTable[(fold * 0x783a9b23) >> 26];
+}
+
 
 #endif /* bitboard_h */
--- breakthrough/main.cpp
+++ breakthrough/main.cpp
@@ -18,16 +18,30 @@ int main(){
     
     struct Board board;
     InitSq120To64(Sq120ToSq64,Sq64ToSq120);
-    PrintBoard(Sq120ToSq64,Sq64ToSq120);
+    //PrintBoard(Sq120ToSq64,Sq64ToSq120);
     
     
     
     US64 playBitBoard = 0ULL;
-    PrintBitBoard(playBitBoard, Sq120ToSq64);
+    //PrintBitBoard(playBitBoard, Sq120ToSq64);
     
+    //printf("%d\n",SQ64(Sq120ToSq64,D2));
     playBitBoard |= (1ULL << SQ64(Sq120ToSq64, D2));
-    printf("D2 Added!\n");
-    PrintBitBoard(playBitBoard, Sq120ToSq64);
+    playBitBoard |= (1ULL << SQ64(Sq120ToSq64, D3));
+    playBitBoard |= (1ULL << SQ64(Sq120ToSq64, D4));
+    
+    int count = countBits(playBitBoard);
+    printf("Count: %d\n",count);
+    
+    int sq64 = 0;
+    while(playBitBoard){
+        sq64 = PopBit(playBitBoard);
+        printf("popped:%d\n",sq64);
+        PrintBitBoard(playBitBoard, Sq120ToSq64);
+    }
+    //printf("%llu\n",playBitBoard);
+    //printf("D2 Added!\n");
+    //PrintBitBoard(playBitBoard, Sq120ToSq64);
     
     
     return 0;
