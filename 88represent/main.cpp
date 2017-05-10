a38d2120f3067f5b0f3bce642cbf7791293afa17
bigchou <40347905S@gmail.com>
2017-05-10 17:50:46 +0800

    reachableornot

--- 88represent/main.cpp
+++ 88represent/main.cpp
@@ -3,7 +3,21 @@
 
 
 int main(){
-	printf("Hello Wolrd");
-	Byte bb[120];
+	printf("Hello Wolrd\n");
+	Byte M[120];
+
+	for(int i=0;i<120;++i)
+		M[i] = i;
+
+	
+	for(int i=0;i<120;++i){
+		printf("%d   ",i);
+		if(!(M[i] & 0x88)){
+			printf("reachable\n");
+		}else{
+			printf("unreachable\n");
+		}
+	}
+
 	return 0;
 }
