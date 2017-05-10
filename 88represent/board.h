252b297ce82993698fc0649bf230a8cd01357c8f
bigchou <40347905S@gmail.com>
2017-05-10 17:24:29 +0800

    0x88_representation[

---
+++ 88represent/board.h
@@ -0,0 +1,7 @@
+
+#ifndef board_h
+#define board_h
+
+typedef unsigned char Byte;
+
+#endif /* board_h */
---
+++ 88represent/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include "board.h"
+
+
+int main(){
+	printf("Hello Wolrd");
+	Byte bb[120];
+	return 0;
+}
